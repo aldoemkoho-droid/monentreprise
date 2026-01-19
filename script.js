33b2e7c209ff594f5ae3313eb0fa22a7f26d6915
aldoemkoho-droid <aldoemkoho@gmail.com>
2026-01-19 12:01:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,26 @@ const menuToggle = document.getElementById("menu-toggle");
 const navMenu = document.getElementById("nav-menu");
 menuToggle.addEventListener("click", () => {
     navMenu.classList.toggle("active");
+
 });
+
+<script>
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement (
+    {
+      pageLanguage: 'fr',
+      includedLanguages: 'fr,en,es,pt,ar',
+      autoDisplay: false
+    },
+    'google_translate_element'
+  );
+}
+function setLanguage(lang) {
+  const select = document.querySelector (".goog-te-combo");
+  if (select) {
+    select.value = lang;
+    select.dispatchEvent (new Event("change"));
+  }
+}
+</script>
+<script src=" https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit "></script>
